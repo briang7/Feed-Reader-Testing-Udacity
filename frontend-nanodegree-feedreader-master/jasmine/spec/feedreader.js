a14ba86238dc9359caa0aba90806032bc1d16ea0
briang7 <briangease7@gmail.com>
2017-10-27 23:08:36 -0400

    Update feedreader.js

--- frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
+++ frontend-nanodegree-feedreader-master/jasmine/spec/feedreader.js
@@ -26,7 +26,7 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -37,7 +37,7 @@ $(function() {
             });
         });
 
-        /* TODO: Write a test that loops through each feed
+        /* This is a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -49,19 +49,11 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
-
-        /* TODO: Write a test that ensures the menu element is
+        /* This is a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
-         */
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        */
     describe("The menu", function() {
 
          it('By defualt the menu is hidden', function() {
@@ -77,9 +69,7 @@ $(function() {
           });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
-
-        /* TODO: Write a test that ensures when the loadFeed
+        /* This is a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -94,14 +84,14 @@ $(function() {
          });
          
          it('feed container isn\'t empty', function() {
-            var entries = $('.entry').length;
+            var entries = $('.feed .entry').length;
             expect(entries).toBeGreaterThan(0);
          });
     });
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
+   
 
-        /* TODO: Write a test that ensures when a new feed is loaded
+        /* This is a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
